9aff572681bef7bc394eb6ec4b1e33cc54bfbafa
Ed Kelley <edwardthev@mac.com>
2019-03-17 23:07:11 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -135,25 +135,25 @@ def __get_pwm_colors__():
     }
 
 
-#def __get_ws2801_colors__():
-#    """
-#    Returns the color codes for a WS2801 based light set on a bright setting.
-#    """
-#
-#100% Bright
-#    return {
-#        weather.RED: (255, 0, 0),
-#        weather.GREEN: (0, 255, 0),
-#        weather.BLUE: (0, 0, 255),
-#        weather.LOW: (255, 0, 255),
-#        weather.OFF: (0, 0, 0),
-#        weather.GRAY: (50, 50, 50),
-#        weather.YELLOW: (255, 255, 0),
-#        weather.DARK_YELLOW: (20, 20, 0),
-#        weather.WHITE: (255, 255, 255)
-#    }
-
 def __get_ws2801_colors__():
+    """
+    Returns the color codes for a WS2801 based light set on a bright setting.
+    """
+
+#100% Bright
+    return {
+        weather.RED: (255, 0, 0),
+        weather.GREEN: (0, 255, 0),
+        weather.BLUE: (0, 0, 255),
+        weather.LOW: (255, 0, 255),
+        weather.OFF: (0, 0, 0),
+        weather.GRAY: (50, 50, 50),
+        weather.YELLOW: (255, 255, 0),
+        weather.DARK_YELLOW: (20, 20, 0),
+        weather.WHITE: (255, 255, 255)
+    }
+
+def __get_ws2801_d_colors__():
     """
     Returns the color codes for a WS2801 based light set on a dim setting.
     """
